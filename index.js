e12e1903cf5b93a28af2e59f448c702c1862aefb
nischal08 <nischal.karanjit@gmail.com>
2019-02-24 22:40:36 +0800

    modified array value

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 class Rest extends React.Component {
-
   friends = [
-    {name:'Sujan', year:2054 , skill:"none"},
-      {name:'Suraj', year:2055 , skill:"guff" },
-      {name:'Kundan', year:2054 , skill:"marriage" },
-      {name:'Mack', year:2053 , skill:"mimic" }
-  ]
+    { name: 'Sujan', year: 2054, skill: 'none' },
+    { name: 'Suraj', year: 2055, skill: 'guff' },
+    { name: 'Kundan', year: 2054, skill: 'yes or no' },
+    { name: 'Mack', year: 2053, skill: 'mimic' }
+  ];
 
   render() {
     return (
@@ -15,12 +14,14 @@ class Rest extends React.Component {
             <h3>{this.props.message}</h3>
           </div>
           <div class='card-body bg-light'>
-          <h4>List of your friends : </h4>
-         {this.friends.map(friends =>(
-         <Person name={friends.name} year={friends.year} skill={friends.skill}/>
-         ))}
-
-          
+            <h4>List of your friends : </h4>
+            {this.friends.map(friends => (
+              <Person
+                name={friends.name}
+                year={friends.year}
+                skill={friends.skill}
+              />
+            ))}
           </div>
         </div>
       </div>
@@ -28,14 +29,13 @@ class Rest extends React.Component {
   }
 }
 
-const Person=(props)=>(
+const Person = props => (
   <div>
-    <span class="mr-1">Name : {props.name} ,</span>
-    <span class="mr-1">Birth Date : {props.year} ,</span>
-    <span class="mr-1">Skill : {props.skill} </span>
+    <span class='mr-1'>Name : {props.name} ,</span>
+    <span class='mr-1'>Birth Date : {props.year} ,</span>
+    <span class='mr-1'>Skill : {props.skill} </span>
   </div>
 );
-
 
 ReactDOM.render(
   <Rest message='Friends Array' />,
