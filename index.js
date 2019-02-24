1c2ab50a5228af7926246a31deed7c1016f21822
nischal08 <nischal.karanjit@gmail.com>
2019-02-25 06:30:11 +0800

    changed color

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ class App extends React.Component {
   friends = [
     { name: 'Sujan', address: 'Chabahil', contact: 9813234434 },
     { name: 'Suraj', address: 'Kalopul', contact: 9841232232 },
-    { name: 'Kundan', address: 'Sundarijal', contact:9860023423 },
-    { name: 'Mack', address: 'Mitrapark', contact:9813621921 }
+    { name: 'Kundan', address: 'Sundarijal', contact: 9860023423 },
+    { name: 'Mack', address: 'Mitrapark', contact: 9813621921 }
   ];
 
   render() {
@@ -11,7 +11,7 @@ class App extends React.Component {
       <div class='container col-md-4  mt-5'>
         <div class='card bg-danger '>
           <div class='card-header bg-danger'>
-            <h3>{this.props.message}</h3>
+            <h3 class='text-center text-white'>{this.props.message}</h3>
           </div>
           <div class='card-body bg-light'>
             <h4>List of your friends : </h4>
